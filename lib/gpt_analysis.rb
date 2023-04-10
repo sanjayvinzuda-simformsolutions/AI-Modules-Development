b4db2cdce09ea35168668e330554711eaae95155
PurvaK <purva.k@simformsolutions.com>
2023-04-10 16:06:28 +0530

    minor updates in lib file

--- lib/gpt_analysis.rb
+++ lib/gpt_analysis.rb
@@ -2,7 +2,7 @@ require "openai"
 
 def analyze_code(code_snippet)
   # Set up OpenAI API key
-  client = OpenAI::Client.new(api_key: Rails.application.credentials.dig(:openAI, :open_ai_api_key))
+  client = OpenAI::Client.new(access_token: Rails.application.credentials.dig(:openAI, :open_ai_api_key))
   
   # Pass the code snippet to the GPT-based AI model
   response = client.completions(parameters: {
@@ -11,7 +11,5 @@ def analyze_code(code_snippet)
     max_tokens: 2000
   })
   # Extract the list of potential issues from the response
-  issues = response.choices[0].text.split("\n")
-  # Return the list of potential issues
-  return issues
+  response["choices"][0]["text"].split("\n")
 end
