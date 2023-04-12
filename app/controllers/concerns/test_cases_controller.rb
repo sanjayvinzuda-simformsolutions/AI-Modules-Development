e9507448ae803c5a31424f30bb968164c47c6238
Rutani Thakkar <rutani.t@simformsolutions.com>
2023-04-12 17:47:15 +0530

    Feat: Added a unit test cases code generator module

---
+++ app/controllers/concerns/test_cases_controller.rb
@@ -0,0 +1,35 @@
+class TestCasesController < ApplicationController
+
+  def show; end
+
+  def generate
+    @code = params[:code]
+    @language = params[:language]
+    @test_cases = generate_test_cases(@code, @language)
+    render :show
+  end
+
+  private
+
+  def generate_test_cases(code, language)
+    prompt = get_prompt(code, language)
+
+    client = OpenAI::Client.new(access_token: "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")
+    response = client.completions(
+      parameters: {
+        model: 'text-davinci-003',
+        prompt: prompt,
+        max_tokens: 1024,
+        best_of: 1,
+        top_p: 1,
+        # n: 1,
+        # stop: "\n"
+      }
+    )
+    response.dig("choices", 0, "text").split("\n")
+  end
+
+  def get_prompt(code, language)
+    "Write unit tests in #{language} using it's most used framework. Here is the file:\n #{code}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "test_cases#show"
+  post 'test_cases/generate', to: 'test_cases#generate', as: 'generate_test_cases'
+
 end
