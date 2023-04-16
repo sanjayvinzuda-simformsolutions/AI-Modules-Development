7045050accb64cceaf58386a34a56bc14a29ccaa
Abhishek-Badmaliya <abhishek.badmaliya@simformsolutions.com>
2023-04-16 17:11:14 +0530

    refactor: refactor the code and design changes

--- app/controllers/open_ai_code_analysis_controller.rb
+++ app/controllers/open_ai_code_analysis_controller.rb
@@ -3,6 +3,8 @@ require 'gpt_analysis'
 class OpenAiCodeAnalysisController < ApplicationController
   def test_cases; end
 
+  def code_docs; end
+
   def generate_test_cases
     code = params[:code]
     language = params[:language]
@@ -11,9 +13,20 @@ class OpenAiCodeAnalysisController < ApplicationController
     render :test_cases
   end
 
+  def generate_code_docs
+    @code = params[:code]
+    prompt = get_code_doc_prompt(@code)
+    @result = analyze_code(prompt)
+    render :code_docs
+  end
+
   private
 
   def get_test_cases_prompt(code, language)
     "Write unit tests in #{language} using it's most used framework. Here is the file:\n #{code}"
+  end
+
+  def get_code_doc_prompt(code)
+    "Generate the code documentation with markdown:\n #{code}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   root "home#index"
   post 'open_ai_code_analysis/generate_test_cases', to: 'open_ai_code_analysis#generate_test_cases', as: 'generate_test_cases'
   get 'test_cases', to: 'open_ai_code_analysis#test_cases', as: 'test_cases'
-  get 'code_doc_generators/generate', to: 'code_doc_generators#index'
-  post 'code_doc_generators/generate', to: 'code_doc_generators#generate'
+  get 'code_docs', to: 'open_ai_code_analysis#code_docs', as: 'code_docs'
+  post 'open_ai_code_analysis/generate_code_docs', to: 'open_ai_code_analysis#generate_code_docs', as: 'genrate_code_docs'
 end
