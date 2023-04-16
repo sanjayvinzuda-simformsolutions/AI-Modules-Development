3306f1281ae3888b9f7ded5104bd37f86eb4bded
Purva Karanjawala <116082151+purvak-simform@users.noreply.github.com>
2023-04-16 18:07:11 +0530

    Revert "Feat/code generator module"

--- app/controllers/open_ai_code_analysis_controller.rb
+++ app/controllers/open_ai_code_analysis_controller.rb
@@ -11,24 +11,9 @@ class OpenAiCodeAnalysisController < ApplicationController
     render :test_cases
   end
 
-  def code_generator; end
-
-  def generate_code
-    code = params[:code]
-    language = params[:language]
-    prompt = get_code_generator_prompt(code, language)
-    @generated_code = analyze_code(prompt)
-    render :code_generator
-  end
-
-
   private
 
   def get_test_cases_prompt(code, language)
     "Write unit tests in #{language} using it's most used framework. Here is the file:\n #{code}"
-  end
-
-  def get_code_generator_prompt(code, language)
-    "write function in #{language} code for #{code}"
   end
 end
