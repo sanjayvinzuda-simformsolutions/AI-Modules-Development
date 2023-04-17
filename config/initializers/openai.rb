f5c941bcdd4e9004ab30e07a6eed2f4ca1d3a82e
PurvaK <purva.k@simformsolutions.com>
2023-04-17 18:42:36 +0530

    Refactor and minor fixes

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = ENV['OPENAI_ACCESS_TOKEN']
+  config.access_token = ENV['OPENAI_ACCESS_TOKEN'] || Rails.application.credentials.dig(:openAI, :open_ai_api_key)
+  OpenAI::Client.new(access_token: api_key)
 end
