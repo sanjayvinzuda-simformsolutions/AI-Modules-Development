3408f2ee64048e7068d5300f2fd7c5a374d5180e
PurvaK <purva.k@simformsolutions.com>
2023-04-17 18:55:11 +0530

    patch error

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,3 @@
 OpenAI.configure do |config|
   config.access_token = ENV['OPENAI_ACCESS_TOKEN'] || Rails.application.credentials.dig(:openAI, :open_ai_api_key)
-  OpenAI::Client.new(access_token: api_key)
 end
